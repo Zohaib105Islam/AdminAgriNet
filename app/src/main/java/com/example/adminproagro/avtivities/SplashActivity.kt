9ac3b7f2d98b92023ba2e49e1fe8af5ce48f7433
Rao Zohaib Islam <zohaibrao105@gmail.com>
2024-02-01 21:30:08 +0500

    Finished project

--- app/src/main/java/com/example/adminproagro/avtivities/SplashActivity.kt
+++ app/src/main/java/com/example/adminproagro/avtivities/SplashActivity.kt
@@ -1,16 +1,50 @@
 package com.example.adminproagro.avtivities
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import com.example.adminproagro.R
+import com.example.adminproagro.databinding.ActivitySplashBinding
 import com.google.firebase.auth.FirebaseAuth
 
 class SplashActivity : AppCompatActivity() {
+
+    val binding : ActivitySplashBinding by lazy {
+        ActivitySplashBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        setContentView(binding.root)
+
+
+        // Load the animation from the XML file
+        val fadeInAnimation = AnimationUtils.loadAnimation(this, R.anim.alphain)
+
+        // Set an optional AnimationListener
+        fadeInAnimation.setAnimationListener(object : Animation.AnimationListener {
+            override fun onAnimationStart(animation: Animation) {
+                // Animation started
+            }
+
+            override fun onAnimationEnd(animation: Animation) {
+                // Animation ended
+            }
+
+            override fun onAnimationRepeat(animation: Animation) {
+                // Animation repeated (if set to repeat)
+            }
+        })
+
+        // Start the animation on your ImageView
+        binding.splashImage.startAnimation(fadeInAnimation)
+
+
+
 
         Handler().postDelayed({
             chechUserExist()
